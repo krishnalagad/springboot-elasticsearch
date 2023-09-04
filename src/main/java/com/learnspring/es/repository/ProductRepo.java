bc5fb80b5956c2498f454565d5b912592bf22556
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-04 16:36:57 +0530

    - dump repo updation

--- src/main/java/com/learnspring/es/repository/ProductRepo.java
+++ src/main/java/com/learnspring/es/repository/ProductRepo.java
@@ -2,6 +2,8 @@ package com.learnspring.es.repository;
 
 import com.learnspring.es.entity.Product;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepo extends ElasticsearchRepository<Product, Integer> {
 }
