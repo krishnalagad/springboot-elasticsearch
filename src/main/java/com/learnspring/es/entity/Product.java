df4f88450611d30c557694de9eaab9a443241b29
Krishna Lagad <krishnalagad2@gmail.com>
2023-09-07 15:24:54 +0530

    - setting type to the field of index.

--- src/main/java/com/learnspring/es/entity/Product.java
+++ src/main/java/com/learnspring/es/entity/Product.java
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Document(indexName = "products")
 @Data
@@ -13,13 +16,18 @@ import org.springframework.data.elasticsearch.annotations.Document;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Product {
 
+    @Id
     private int id;
 
+    @Field(type = FieldType.Text)
     private String name;
 
+    @Field(type = FieldType.Text)
     private String description;
 
+    @Field(type = FieldType.Integer)
     private int quantity;
 
+    @Field(type = FieldType.Double)
     private double price;
 }
